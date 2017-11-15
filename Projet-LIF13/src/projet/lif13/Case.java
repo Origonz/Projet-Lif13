e3040d19a933e6da4d616835c96a2e65938f9a58
Billy <vizaar@hotmail.fr>
2017-11-15 14:22:45 +0100

    Modele debut

---
+++ Projet-LIF13/src/projet/lif13/Case.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.lif13;
+
+/**
+ *
+ * @author billy
+ */
+public class Case {
+    private int id;
+    
+    Case(){
+        id=0;
+    }
+    
+    Case(int i){
+        id=i;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
