899799782fbf4173b5859ee7d6ca651caf5ae5ae
Zonthem <nico7.garaud@gmail.com>
2017-11-15 14:58:53 +0100

    Debut Vue

---
+++ Projet-LIF13/src/projet/vue/Game.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projet.vue;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import projet.lif13.Grille;
+import projet.lif13.Case;
+
+/**
+ *
+ * @author Zonthem
+ */
+public class Game extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Grille gr = new Grille(6, 3);
+        Node n;
+        
+        GridPane gPane = new GridPane();
+        
+        for (int i = 0; i < gr.getLongueur(); i++) {
+            for (int j = 0; j < gr.getLargeur(); j++) {
+                n = new Text(Integer.toString(((Case)gr[i][j]).getId()));
+                gPane.add(((Case)gr[i][j]).getId(), i, j);
+            }
+        }
+        
+        //root.getChildren().add();
+        
+        Scene scene = new Scene(gPane, 300, 250);
+        
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
