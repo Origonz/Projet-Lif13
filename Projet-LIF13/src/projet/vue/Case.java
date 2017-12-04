454ddc31a7b802e4c66ca068f97dba8a5a08e5ee
Zonthem <nico7.garaud@gmail.com>
2017-12-04 10:21:50 +0100

    Reset ET Restart AVEC resize des images selon taille de la grille

--- Projet-LIF13/src/projet/vue/Case.java
+++ Projet-LIF13/src/projet/vue/Case.java
@@ -8,6 +8,7 @@ package projet.vue;
 import java.util.ArrayList;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import projet.controleur.Controleur;
 
 /** 
  * Element graphique du jeu.
@@ -17,7 +18,6 @@ public class Case extends ImageView {
     
     private int X_correspondant;
     private int Y_correspondant;
-    private int etat;
     private ArrayList<Image> images;
     
     /**
@@ -27,37 +27,34 @@ public class Case extends ImageView {
      * @param x correspondance X dans la matrice de controle
      * @param y correspondance Y dans la matrice de controle
      */
-    public Case(int type, int x, int y) {
-        super(new Image("projet/ressources/defaut.png"));
+    public Case(int type, int x, int y, int ratio) {
+        super(new Image("projet/ressources/defaut.png", 100*4/ratio, 100*4/ratio, false, false));
         X_correspondant = x;
         Y_correspondant = y;
         if (type == 0) {
-            etat = 0;
             try {
                 images = new ArrayList<>();
-                images.add(new Image("projet/ressources/defaut.png"));
-                images.add(new Image("projet/ressources/horizontal.png"));
-                images.add(new Image("projet/ressources/vertical.png"));
-                images.add(new Image("projet/ressources/topleft.png"));
-                images.add(new Image("projet/ressources/botleft.png"));
-                images.add(new Image("projet/ressources/topright.png"));
-                images.add(new Image("projet/ressources/botright.png"));
+                images.add(new Image("projet/ressources/defaut.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/horizontal.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/vertical.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/topleft.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/botleft.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/topright.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/botright.png", 100*4/ratio, 100*4/ratio, false, false));
             } catch (Exception e) {
                 System.err.println(e.getMessage() + " - " + images);
             }
         } else {
-            etat = type;
             try {
                 images = new ArrayList<>();
-                images.add(new Image("projet/ressources/b1.png"));
-                images.add(new Image("projet/ressources/b2.png"));
-                images.add(new Image("projet/ressources/b3.png"));
+                images.add(new Image("projet/ressources/b1.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/b2.png", 100*4/ratio, 100*4/ratio, false, false));
+                images.add(new Image("projet/ressources/b3.png", 100*4/ratio, 100*4/ratio, false, false));
+                setImage(images.get(0));
             } catch (Exception e) {
                 System.err.println(e.getMessage() + " - " + images);
             }
-        }
-        
-        
+        }  
     }
     
     public void setCoord(int x, int y) {
@@ -70,22 +67,18 @@ public class Case extends ImageView {
         return (tab);
     }
     
-    public int getEtat() {
-        return etat;
-    }
-    
-    public void changeEtat() {
-        //if jouerCoup
-        if (etat < 6) etat++;
-        else if (etat == 6) etat = 0;
-        //autres cas : ne pas changer l'etat
-        
-        setImage(images.get(etat));
+    public void changeEtat(Controleur c) {
+        if (c.jouerCoup(X_correspondant, Y_correspondant)) {
+            c.setLastCase(this);
+            setImage(images.get(c.getIdGraphiqueCase(X_correspondant, Y_correspondant)));
+        }
     }
 
-    public void reset() {
-        //if resetable
-        etat = 0;
-        setImage(images.get(etat));
+    void chargeDefaut() {
+        setImage(images.get(0));
+    }
+
+    public void changeEtatAdmin(Controleur c) {
+        setImage(images.get(c.getIdGraphiqueCase(X_correspondant, Y_correspondant)));
     }
 }
